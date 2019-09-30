b074bceadca8209c7686e5977265ca4c51d87cd2
noracyrd <norabyrd@alumni.upenn.edu>
2019-09-30 17:29:29 -0500

    incorporated moment, and added a default case for movies

--- liri.js
+++ liri.js
@@ -12,7 +12,7 @@ const spotify = new spotifyAPI({
 const fs = require("fs");
 
 // Including the Moment package
-//const moment = require("moment");
+var moment = require('moment');
 
 // Including the Axios package
 const axios = require("axios");
@@ -36,8 +36,7 @@ let concertSearch = function () {
                     for (var i in response.data) {
                         console.log(response.data[i].venue.name);
                         console.log(response.data[i].venue.city);
-                        // need to convert below via moment
-                        console.log(response.data[i].datetime);
+                        console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
                         console.log("--------");
                     }
                 }).catch(function (error) {
@@ -71,7 +70,7 @@ let concertSearch = function () {
                         console.log(response.data[i].venue.name);
                         console.log(response.data[i].venue.city);
                         // need to convert below via moment
-                        console.log(response.data[i].datetime);
+                        console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
                         console.log("--------");
                     }
 
@@ -103,10 +102,6 @@ let concertSearch = function () {
 
 // Function to search Spotify for song information
 let songSearch = function () {
-    let songArtist;
-    let songName;
-    let songLink;
-    let songAlbum;
     switch (input) {
         case undefined:
             let song = "Never Gonna Give You Up";
@@ -153,6 +148,42 @@ let songSearch = function () {
 // Function to search OMDB for movie information
 let movieSearch = function () {
     // axios call to get the information we need about the movie
+    switch(input) {
+        case undefined:
+                axios.get("http://www.omdbapi.com/", {
+                    params: {
+                        apikey: keys.omdb.id,
+                        t: "Rick Astley: Never Gonna Give You Up",
+                    }
+                }).then(function (response) {
+                    console.log(response.data.Title + " was released in " + response.data.Year + ".");
+                    console.log("It starred the following actors: " + response.data.Actors);
+                    console.log("The plot: " + response.data.Plot);
+                    
+                    console.log("Country: " + response.data.Country);
+
+                }).catch(function (error) {
+                    if (error.response) {
+                        // The request was made and the server responded with a status code
+                        // that falls out of the range of 2xx
+                        console.log("---------------Data---------------");
+                        console.log(error.response.data);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.status);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.headers);
+                    } else if (error.request) {
+                        // The request was made but no response was received
+                        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                        console.log(error.request);
+                    } else {
+                        // Something happened in setting up the request that triggered an Error
+                        console.log("Error", error.message);
+                    }
+                    console.log(error.config);
+                })
+                break;
+        default:
     axios.get("http://www.omdbapi.com/", {
         params: {
             apikey: keys.omdb.id,
@@ -185,6 +216,8 @@ let movieSearch = function () {
         }
         console.log(error.config);
     })
+    break;
+}
 }
 
 // Switch cases for the various Liri commands
