1298fb40eaa2040b18dd94bc48bbefa034d5f7dd
noracyrd <norabyrd@alumni.upenn.edu>
2019-10-01 17:34:35 -0500

    added a function to make a log of search results for the concert default scenario - need to build it out for the rest :)

--- liri-advanced.js
+++ liri-advanced.js
@@ -21,7 +21,7 @@ const axios = require("axios");
 // Variables to grab user search type and search value input
 // let search = process.argv[2].toLowerCase().trim();
 // let input = process.argv[3];
-var input = "";
+
 // function to get user input wihtout argv
 
 var getError = function (error) {
@@ -45,152 +45,161 @@ var getError = function (error) {
     console.log(error.config);
 }
 
+var logLiri = function (newData) {
+    fs.appendFile("log.txt", newData, function (err) {
+        // If an error was experienced we will log it.
+        if (err) {
+            console.log(err);
+        }
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
 
+    });
+}
 // Functions for each Liri command case
 
 // Function to search Bands In Town for concert information
-let concertSearch = function () {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What would you like to search?",
-                name: "input"
-            }
-        ]).then(function (inquirerResponse) {
-            switch (inquirerResponse.input) {
-                case "":
-                    let artist = "Rick Astley";
-                    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bandsintown.id + "")
-                        .then(function (response) {
-                            console.log("You didn't pick an artist so you get " + artist + ". This is why we can't have nice things!");
-                            console.log("--------");
-                            for (var i in response.data) {
-                                console.log(response.data[i].venue.name);
-                                console.log(response.data[i].venue.city);
-                                console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
-                                console.log("--------");
-                            }
-                        }).catch(getError);
-                    break;
-                default:
-                    axios.get("https://rest.bandsintown.com/artists/" + inquirerResponse.input + "/events?app_id=codingbootcamp")
-                        .then(function (response) {
-                            console.log("Upcoming concert info for " + inquirerResponse.input + ":");
-                            console.log("--------");
-                            for (var i in response.data) {
-                                console.log(response.data[i].venue.name);
-                                console.log(response.data[i].venue.city);
-                                // need to convert below via moment
-                                console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
-                                console.log("--------");
-                            }
+let concertSearch = function (concert) {
+    switch (concert) {
+        case "":
+            let artist = "Rick Astley";
+            axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bandsintown.id + "")
+                .then(function (response) {
+                    console.log("You didn't pick an artist so you get " + artist + ". This is why we can't have nice things!");
+                    console.log("--------");
+                    logLiri(`Concert search results for ${artist}: \n ******** \n`);
+                    for (var i in response.data) {
+                        console.log(`${response.data[i].venue.name} \n ${response.data[i].venue.city} \n ${(moment(response.data[i].datetime).format("MM/DD/YYYY"))} \n --------`);
+                        logLiri(`${response.data[i].venue.name} \n ${response.data[i].venue.city} \n ${(moment(response.data[i].datetime).format("MM/DD/YYYY"))} \n -------- \n`);
+                    }
+                }).catch(getError);
+            break;
+        default:
+            axios.get("https://rest.bandsintown.com/artists/" + concert + "/events?app_id=codingbootcamp")
+                .then(function (response) {
+                    console.log("Upcoming concert info for " + concert + ":");
+                    console.log("--------");
+                    for (var i in response.data) {
+                        console.log(response.data[i].venue.name);
+                        console.log(response.data[i].venue.city);
+                        // need to convert below via moment
+                        console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                        console.log("--------");
+                    }
 
-                        }).catch(getError);
-                    break;
-            }
-        }
-        )
+                }).catch(getError);
+            break;
+    }
 }
+
+
 
 
 
 // Function to search Spotify for song information
-let songSearch = function () {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What would you like to search?",
-                name: "input"
-            }
-        ]).then(function (inquirerResponse) {
-            switch (inquirerResponse.input) {
-                case "":
-                    let song = "Never Gonna Give You Up";
-                    spotify
-                        .search({
-                            type: 'track',
-                            query: song,
-                            limit: 1,
-                        })
-                        .then(function (response) {
-                            console.log("GOTCHA!");
-                            console.log("Artist: " + response.tracks.items[0].artists[0].name);
-                            console.log("Song Name: " + response.tracks.items[0].name);
-                            console.log("Song Preview Link: " + response.tracks.items[0].preview_url);
-                            console.log("Album Name: " + response.tracks.items[0].album.name);
-                        })
-                        .catch(getError);
-                    break;
-                default:
-                    spotify
-                        .search({
-                            type: 'track',
-                            query: inquirerResponse.input,
-                            limit: 5,
-                        })
-                        .then(function (response) {
-                            for (var i in response.tracks.items) {
-                                console.log("Artist: " + response.tracks.items[i].artists[0].name);
-                                console.log("Song Name: " + response.tracks.items[i].name);
-                                console.log("Song Preview Link: " + response.tracks.items[i].preview_url);
-                                console.log("Album Name: " + response.tracks.items[i].album.name);
-                                console.log("--------");
-                            }
-                        })
-                        .catch(getError);
-                    break;
-            }
-        })
+let songSearch = function (song) {
+
+    switch (song) {
+        case "":
+            let songDefault = "Never Gonna Give You Up";
+            spotify
+                .search({
+                    type: 'track',
+                    query: songDefault,
+                    limit: 1,
+                })
+                .then(function (response) {
+                    console.log("GOTCHA!");
+                    console.log("Artist: " + response.tracks.items[0].artists[0].name);
+                    console.log("Song Name: " + response.tracks.items[0].name);
+                    console.log("Song Preview Link: " + response.tracks.items[0].preview_url);
+                    console.log("Album Name: " + response.tracks.items[0].album.name);
+                })
+                .catch(getError);
+            break;
+        default:
+            spotify
+                .search({
+                    type: 'track',
+                    query: song,
+                    limit: 5,
+                })
+                .then(function (response) {
+                    for (var i in response.tracks.items) {
+                        console.log("Artist: " + response.tracks.items[i].artists[0].name);
+                        console.log("Song Name: " + response.tracks.items[i].name);
+                        console.log("Song Preview Link: " + response.tracks.items[i].preview_url);
+                        console.log("Album Name: " + response.tracks.items[i].album.name);
+                        console.log("--------");
+                    }
+                })
+                .catch(getError);
+            break;
+    }
 }
+
 
 
 // Function to search OMDB for movie information
-let movieSearch = function () {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What would you like to search?",
-                name: "input"
-            }
-        ]).then(function (inquirerResponse) {
-            // axios call to get the information we need about the movie
-            switch (inquirerResponse.input) {
-                case "":
-                    axios.get("http://www.omdbapi.com/", {
-                        params: {
-                            apikey: keys.omdb.id,
-                            t: "Rick Astley: Never Gonna Give You Up",
-                        }
-                    }).then(function (response) {
-                        console.log(response.data.Title + " was released in " + response.data.Year + ".");
-                        console.log("It starred the following actors: " + response.data.Actors);
-                        console.log("The plot: " + response.data.Plot);
+let movieSearch = function (movie) {
 
-                        console.log("Country: " + response.data.Country);
+    // axios call to get the information we need about the movie
+    switch (movie) {
+        case "":
+            axios.get("http://www.omdbapi.com/", {
+                params: {
+                    apikey: keys.omdb.id,
+                    t: "Rick Astley: Never Gonna Give You Up",
+                }
+            }).then(function (response) {
+                console.log(response.data.Title + " was released in " + response.data.Year + ".");
+                console.log("It starred the following actors: " + response.data.Actors);
+                console.log("The plot: " + response.data.Plot);
 
-                    }).catch(getError);
-                    break;
-                default:
-                    axios.get("http://www.omdbapi.com/", {
-                        params: {
-                            apikey: keys.omdb.id,
-                            t: inquirerResponse.input,
-                        }
-                    }).then(function (response) {
-                        console.log(response.data.Title + " was released in " + response.data.Year + ".");
-                        console.log("It starred the following actors: " + response.data.Actors);
-                        console.log("The plot: " + response.data.Plot);
-                        console.log("Ratings: IMDB: " + response.data.imdbRating + "; Rotten Tomatoes: " + response.data.Ratings[1].Value);
-                        console.log("Country: " + response.data.Country);
-                        // still need to include default case
-                    }).catch(getError);
-                    break;
-            }
-        })
+                console.log("Country: " + response.data.Country);
+
+            }).catch(getError);
+            break;
+        default:
+            axios.get("http://www.omdbapi.com/", {
+                params: {
+                    apikey: keys.omdb.id,
+                    t: movie,
+                }
+            }).then(function (response) {
+                console.log(response.data.Title + " was released in " + response.data.Year + ".");
+                console.log("It starred the following actors: " + response.data.Actors);
+                console.log("The plot: " + response.data.Plot);
+                console.log("Ratings: IMDB: " + response.data.imdbRating + "; Rotten Tomatoes: " + response.data.Ratings[1].Value);
+                console.log("Country: " + response.data.Country);
+                // still need to include default case
+            }).catch(getError);
+            break;
+    }
 }
+
+let theThing = function (text) {
+    fs.readFile(text, "utf8", function (error, data) {
+        if (error) {
+            getError;
+        }
+        console.log(data);
+        var randomLiri = data.split(",");
+        var [randomLiriCommand, randomLiriSearch] = randomLiri;
+        switch (randomLiriCommand) {
+            case "concert-this":
+                concertSearch(randomLiriSearch);
+                break;
+            case "spotify-this-song":
+                songSearch(randomLiriSearch);
+                break;
+            case "movie-this":
+                movieSearch(randomLiriSearch);
+                break;
+        }
+    })
+}
+
+
 inquirer
     .prompt([
         {
@@ -199,22 +208,26 @@ inquirer
             choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
             name: "liriCommand"
         },
+        {
+            type: "input",
+            message: "What would you like to search?",
+            name: "input"
+        },
     ]).then(function (inquirerResponse) {
         // Switch cases for the various Liri commands
         console.log(typeof inquirerResponse.liriCommand);
         switch (inquirerResponse.liriCommand) {
             case "concert-this":
-                concertSearch();
-
+                concertSearch(inquirerResponse.input);
                 break;
             case "spotify-this-song":
-                songSearch();
+                songSearch(inquirerResponse.input);
                 break;
             case "movie-this":
-                movieSearch();
+                movieSearch(inquirerResponse.input);
                 break;
             case "do-what-it-says":
-                theThing();
+                theThing(inquirerResponse.input);
                 break;
         }
     });
